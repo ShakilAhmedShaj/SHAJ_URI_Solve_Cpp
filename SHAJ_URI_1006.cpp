16c4cfbd532951ee1e8d42a0d717fc69ee4c95ad
Shakil Ahmed Shaj <shakilahmedshaj@gmail.com>
2018-01-22 02:34:45 +0600

    no message

--- SHAJ_URI_1006.cpp
+++ SHAJ_URI_1006.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-
 int main()
 {
         float a,b,c,media;
@@ -10,6 +9,4 @@ int main()
                 media= ((a*2)+(b*3)+(c*5))/10;
                 cout<<"MEDIA = "<<fixed<<setprecision(1)<<media<<endl;
         }
-
-
 }
